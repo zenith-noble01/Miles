26728647034943d1497765eb567b341af4e752c2
zenith-noble01 <louisefongang@gmail.com>
2021-08-27 01:15:51 +0100

    realtime chat functionality added

--- client/src/Messenger.jsx
+++ client/src/Messenger.jsx
@@ -20,7 +20,7 @@ export default function Messenger() {
   const scrollRef = useRef();
 
   useEffect(() => {
-    socket.current = io("ws://localhost:7000");
+    socket.current = io("ws://localhost:8900");
     socket.current.on("getMessage", (data) => {
       setArrivalMessage({
         sender: data.senderId,
